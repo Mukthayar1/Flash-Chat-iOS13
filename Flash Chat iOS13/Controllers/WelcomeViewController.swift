0a2941cd80c2ccabf6691afacdf902b20639a2ce
Muhammad Mukthayar <khanadil1000s@gmail.com>
2024-10-07 14:52:05 +0500

    working ion

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -11,11 +11,18 @@ import UIKit
 class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
+    let character = "⚡️FlashChat"
+    var characterIndex = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        for letter in character {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * characterIndex, repeats: false) { Timer in
+                self.titleLabel.text?.append(letter)
+            }
+            characterIndex += 1
+        }
     }
     
 
