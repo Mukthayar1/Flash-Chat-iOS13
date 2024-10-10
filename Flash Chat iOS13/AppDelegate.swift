a7acd125e50c8304672d610605127f0c31a87f51
Muhammad Mukthayar <khanadil1000s@gmail.com>
2024-10-10 11:56:10 +0500

    w

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import FirebaseCore
+import FirebaseFirestore
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,7 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        FirebaseApp.configure()
+        FirebaseApp.configure();
+        let db = Firestore.firestore();
+        print(db)
         return true
     }
 
--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -1,40 +1,75 @@
 import UIKit
+import FirebaseFirestoreInternal
 import FirebaseAuth
 import Toaster
 
 class ChatViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
+    let db = Firestore.firestore();
     
-    var message : [Message] = [
-        Message(sender:"abc@gmail.com",body:"there ?"),
-        Message(sender:"abc@gmail.com",body:"Please reply ?"),
-        Message(sender:"muk@gmail.com",body:"Yes ?"),
-        Message(sender:"abc@gmail.com",body:"No ?"),
-        Message(sender:"abc@gmail.com",body:"Why ?"),
-    ]
+    var message : [Message] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = Constants.appName
         navigationItem.hidesBackButton = true
         tableView.dataSource = self
-        tableView.register(UINib(nibName: Constants.cellNibName, bundle: nil), forCellReuseIdentifier:Constants.cellIdentifier)
-//        tableView.delegate = self
+        tableView.register(UINib(nibName: Constants.cellNibName, bundle: nil), forCellReuseIdentifier:Constants.cellIdentifier);
+        loadMessage();
+        //      tableView.delegate = self
+    }
+    
+    func loadMessage(){
+        db.collection(Constants.FStore.collectionName)
+            .order(by: Constants.FStore.dateField)
+            .addSnapshotListener { querySnapshot, error in
+            if let e = error {
+                print("There was error \(e)")
+            } else {
+                self.message = [];
+                if let snapshotDocument = querySnapshot?.documents {
+                    for doc in snapshotDocument {
+                        let data = doc.data();
+                        if let sender = data[Constants.FStore.senderField] as? String , let body = data[Constants.FStore.bodyField] as? String {
+                            let newMessage = Message(sender: sender, body: body);
+                            self.message.append(newMessage);
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        }
+                        
+                    }
+                }
+            }
+        }
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        if  let messageBody = messageTextfield.text , let messageSender = Auth.auth().currentUser?.email {
+            db.collection(Constants.FStore.collectionName).addDocument(data: [
+                Constants.FStore.senderField:messageSender,
+                Constants.FStore.bodyField:messageBody,
+                Constants.FStore.dateField:Date().timeIntervalSince1970
+            ]) { (error) in
+                if let e = error {
+                    print("eeee",e)
+                } else {
+                    print("Message sended")
+                }
+            }
+        }
     }
     
-
+    
     @IBAction func logoutUser(_ sender: Any) {
         let firebaseAuth = Auth.auth()
         do {
-          try firebaseAuth.signOut()
+            try firebaseAuth.signOut()
             navigationController?.popToRootViewController(animated: true)
         } catch let signOutError as NSError {
-          print("Error signing out: %@", signOutError)
+            print("Error signing out: %@", signOutError)
         }
     }
 }
